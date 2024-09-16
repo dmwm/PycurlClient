0213418f3ca43d63d867ee34630c2e9c1354ae12
Valentin Kuznetsov <vkuznet@gmail.com>
2024-09-16 13:51:59 -0400

    New tag for the package

--- setup4pypi.py
+++ setup4pypi.py
@@ -6,7 +6,7 @@ import setuptools
 from setuptools import setup
 
 # version of pycurl-client, should match current DBS release tag
-package_version = "3.18.2"
+package_version = "3.18.3"
 
 # Requirements file for pip dependencies
 requirements = "requirements.txt"
